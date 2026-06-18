6b001d0b89847d28c24c1e05e23678f6cfe39795
Christyan Simbolon <sambungdata@gmail.com>
2026-06-18 21:11:38 +0700

    feat-backend: add documents urls

---
+++ backend/apps/documents/urls.py
@@ -0,0 +1,11 @@
+"""
+RumahAsri — Documents URLs
+"""
+
+from django.urls import path
+
+from .views import DocumentListView
+
+urlpatterns = [
+    path("", DocumentListView.as_view(), name="document-list"),
+]
